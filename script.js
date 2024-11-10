24fde4d38714bbecf1ddd4b68c3cdd903aeb4177
karter2312 <tafadzwa.mukata@mds.ac.nz>
2024-11-10 21:59:15 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-<script src="script.js"></script>
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
